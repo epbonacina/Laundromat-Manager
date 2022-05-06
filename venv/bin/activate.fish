b5c62bc086e78efe3ee3479c0b054de3cd78d72d
Enzo Pedro Bonacina <epbonacina@gmail.com>
2022-05-06 16:48:35 -0300

    migration do banco de dados

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/enzo/Desktop/Projeto para Lavanderia/venv"
+set -gx VIRTUAL_ENV "/home/enzo/Desktop/Laundromat-Manager/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
